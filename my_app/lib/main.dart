2d873637e41da9feba5bbc50c6e54f894af1e1ba
sandip <sandipbharati07@gmail.com>
2022-07-01 16:36:52 +0545

    updated rms login and validation

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_null_comparison
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:dio/dio.dart';
@@ -20,6 +22,10 @@ class MyApp extends StatelessWidget {
 
 var userName = TextEditingController();
 var passWord = TextEditingController();
+final controller = TextEditingController();
+bool isButtonActive = false;
+//bool onPressed = true;
+bool submit = false;
 final Dio _dio = Dio();
 
 class LoginPage extends StatefulWidget {
@@ -39,9 +45,9 @@ class _LoginPageState extends State<LoginPage> {
         width: double.infinity,
         decoration: BoxDecoration(
           gradient: LinearGradient(begin: Alignment.topCenter, colors: [
-            Colors.cyan.shade400,
-            Colors.cyan.shade300,
-            Colors.cyan.shade200,
+            Colors.blue.shade400,
+            Colors.blue.shade300,
+            Colors.blue.shade200,
           ]),
         ),
         child: Column(
@@ -75,34 +81,86 @@ class InputField extends StatefulWidget {
 class _InputFieldState extends State<InputField> {
   // var userName = TextEditingController();
   // var passWord = TextEditingController();
+  bool _isObscure = true;
+  final _formKey = GlobalKey<FormState>();
+
+  //late TextEditingController controller;
+  final myController = TextEditingController();
   @override
+  void initState() {
+    super.initState();
+    myController.addListener(() {
+      setState(() {
+        submit = myController.text.isNotEmpty;
+      });
+    });
+  }
+
   Widget build(BuildContext context) {
-    return Expanded(
+    return Form(
+      onChanged: () =>
+          setState(() => isButtonActive = _formKey.currentState!.validate()),
+      key: _formKey,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
+          // Text(
+          //   'Username',
+          //   textAlign: TextAlign.justify,
+          //   style: TextStyle(color: Color.fromARGB(255, 192, 189, 215)),
+          // ),
           Container(
+            alignment: Alignment.centerLeft,
             padding: const EdgeInsets.all(10),
             decoration: BoxDecoration(
                 border:
                     Border(bottom: BorderSide(color: Colors.grey.shade200))),
-            child: TextField(
+            child: TextFormField(
               controller: userName,
               decoration: InputDecoration(
+                labelText: 'Username',
                 hintText: "Enter your Username",
                 hintStyle: TextStyle(color: Colors.grey),
                 border: InputBorder.none,
               ),
+              autovalidateMode: AutovalidateMode.always,
+
+              //  print(value);
+              //  print('validator');
+              validator: (value) => value!.length < 10
+                  ? 'Number must be at least 10 digits'
+                  : // return an error message
+                  null,
             ),
           ),
+
+          // Text(
+          //   'Password',
+          //   style: TextStyle(color: Color.fromARGB(255, 192, 189, 215)),
+          // ),
           Container(
             padding: const EdgeInsets.all(10),
             decoration: BoxDecoration(
                 border:
                     Border(bottom: BorderSide(color: Colors.grey.shade200))),
-            child: TextField(
+            child: TextFormField(
+              obscureText: _isObscure,
               controller: passWord,
               decoration: InputDecoration(
+                labelText: 'Password',
                 hintText: "Enter your Password",
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _isObscure = !_isObscure;
+                    });
+                  },
+                  icon: Icon(
+                    // Based on passwordVisible state choose the icon
+                    _isObscure ? Icons.visibility : Icons.visibility_off,
+                    color: Theme.of(context).primaryColorDark,
+                  ),
+                ),
                 hintStyle: TextStyle(color: Colors.grey),
                 border: InputBorder.none,
               ),
@@ -180,26 +238,78 @@ class _InputWrapperState extends State<InputWrapper> {
   }
 }
 
+var con = userName != null && passWord != null;
+
 class Button extends StatefulWidget {
   @override
   State<Button> createState() => _ButtonState();
 }
 
 class _ButtonState extends State<Button> {
+  get yourCallback => null;
+
+  // bool isButtonActive = true;
+  // bool isEmpty(String string) => string.isEmpty;
+  // bool isActive() {
+  //   if (isEmpty(userName.text) == true && isEmpty(passWord.text) == true) {
+  //     //  isInactive();
+  //     // return isButtonActive = false;
+  //     return isButtonActive = false;
+  //   } else {
+  //     return isButtonActive = true;
+  //   }
+  // }
+
+  // bool isInactive() {
+  //   return isButtonActive = false;
+  // }
+
   @override
+  // ignore: curly_braces_in_flow_control_structures
   Widget build(BuildContext context) {
+    // isEmpty == true;
+    print('calling built');
+    // if
+    //     // ignore: curly_braces_in_flow_control_structures
+    //     (isEmpty(userName.text) == true && isEmpty(passWord.text) == true) {
+    //   isInactive();
+    //   //isButtonActive = false;
+    //   // print(isButtonActive);
+    //   // print(!isEmpty(userName.text));
+    //   //  print(passWord.text);
+    //   //return isButtonActive;
+    // }
+    // isActive();
+    // // print(isButtonActive);
+    // if (isEmpty(userName.text) == false && isEmpty(passWord.text) == false) {
+    //   // print(passWord.text);
+
+    //   // isButtonActive = true;
+    //   isActive();
+    // }
+
+    // } else {
+    //   isButtonActive = false;
+    // }
     return Column(
       children: [
-        // ignore: deprecated_member_use
-        OutlinedButton.icon(
-          onPressed: () {
-            Login();
-          },
+//         // ignore: deprecated_member_use
+// if(userName.text==null && passWord.text==null ){
+//     isButtonActive= false;
+// }else{
+//   isButtonActive = true;
+// },
+        ElevatedButton.icon(
+          // onPressed: isButtonActive == true ? yourCallback : null,
+          style: ElevatedButton.styleFrom(
+            onSurface: Colors.blue,
+          ),
           icon: Icon(
             Icons.login,
             size: 18,
           ),
           label: Text('Login'),
+          onPressed: isButtonActive ? () => Login : null,
         ),
       ],
     );
@@ -223,6 +333,36 @@ class _ButtonState extends State<Button> {
 //   }
 //}
 
+// show the dialog
+// Future<void> showMyDialog(data) async {
+//   var context;
+//   return showDialog<void>(
+//     context: context,
+//     barrierDismissible: false, // user must tap button!
+//     builder: (BuildContext context) {
+//       return AlertDialog(
+//         title: const Text('AlertDialog Title'),
+//         content: SingleChildScrollView(
+//           child: ListBody(
+//             children: const <Widget>[
+//               Text('This is a demo alert dialog.'),
+//               Text('Would you like to approve of this message?'),
+//             ],
+//           ),
+//         ),
+//         actions: <Widget>[
+//           TextButton(
+//             child: const Text('Approve'),
+//             onPressed: () {
+//               Navigator.of(context).pop();
+//             },
+//           ),
+//         ],
+//       );
+//     },
+//   );
+// }
+
 Future Login() async {
   try {
     print('userName' + userName.text);
@@ -237,23 +377,43 @@ Future Login() async {
         },
       ),
     );
-    print('sbdjskcjsdncjsndkjcnskdc ');
+
+    // print('sbdjskcjsdncjsndkjcnskdc ');
     print(response);
+    // print(response('userName'));
     return response;
-    var context;
-    if (response.statusCode == 200) {
-      MaterialPageRoute(builder: (context) => SecondRoute());
-    } else if (response.statusCode == 401) {
-      return Text('Invalid credentials');
-    } else {
-      throw Exception('An unknown error occurred');
-    }
-    //returns the successful user data json object
-    print('sdcsdsdcsdsxas ' + response.data);
-    return response.data;
+
+    // if (response.data['userName'] != ' $userName') {
+    //   AlertDialog(actions: [
+    //     Text('Username not match'),
+    //   ]);
+    // }
+
+    // if (response.data['passWord'] != ' $passWord') {
+    //   AlertDialog(actions: [
+    //     Text('PassWord not match'),
+    //   ]);
+    // }
+
+    // if (response.data['userName'] == '$userName' &&
+    //     response.data['passWord'] == '$passWord') {
+    //   AlertDialog(actions: [
+    //     Text('Success'),
+    //   ]);
+    // }
   } on DioError catch (e) {
+    // var context;
+
+    //print(showAlertDialog(e.response!.data));
     //returns the error object if any
-   // print('aaaaaaaaaaaaaaaaaaaaaaaaa ' + e.toString());
-    return e.response!.data;
+    print('adxjdixid');
+
+    // AlertDialog(actions: [
+    //   Text(
+    //     e.response!.data['message'],
+    //   ),
+    // ]);
+    print(e.response!.data['message']);
+    return e.response!.data['message'];
   }
 }
